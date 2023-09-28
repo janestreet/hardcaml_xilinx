7191af9f4de3b9bd3b067c20b8ff359cf8d3c874
public-release <as-public-release@janestreet.com>
2023-09-28 11:21:21 +0100

    v0.17~preview.128.41+46

--- src/fifo_async.ml
+++ src/fifo_async.ml
@@ -24,20 +24,52 @@ let create
     | Block | Auto | Distributed -> ());
   match (build_mode : Build_mode.t) with
   | Simulation ->
-    (* Unfortunately, we cannot properly simulate asynchronous fifos. The choice of
-       clock here is arbitrary *)
-    Fifo.create
-      ?scope
-      ?nearly_full
-      ?nearly_empty
-      ~showahead
-      ()
-      ~capacity
-      ~clock:read_clock
-      ~clear
-      ~rd:read
-      ~wr:write
-      ~d
+    (* Our async fifo implementation currently only supports showahead mode; in the
+       non-showahead case, we just stub in a regular fifo for simulations with an
+       arbitrary clock chosen. *)
+    if showahead
+    then
+      let open Signal in
+      let module Async_fifo =
+        Async_fifo.Make (struct
+          let width = Signal.width d
+          let log2_depth = Int.ceil_log2 capacity
+        end)
+      in
+      let async_fifo =
+        Async_fifo.create
+          ?scope
+          { clock_write = write_clock
+          ; clock_read = read_clock
+          ; reset_write = clear
+          ; reset_read = gnd
+          ; data_in = d
+          ; write_enable = write
+          ; read_enable = read
+          }
+      in
+      { Fifo.q = async_fifo.data_out
+      ; full = async_fifo.full
+      ; empty = ~:(async_fifo.valid)
+      ; nearly_full = gnd
+      ; nearly_empty = async_fifo.almost_empty
+      ; used = gnd
+      ; rd_rst_busy = gnd
+      ; wr_rst_busy = gnd
+      }
+    else
+      Fifo.create
+        ?scope
+        ?nearly_full
+        ?nearly_empty
+        ~showahead
+        ()
+        ~capacity
+        ~clock:read_clock
+        ~clear
+        ~rd:read
+        ~wr:write
+        ~d
   | Synthesis ->
     Xpm_fifo_async.create
       ?fifo_memory_type
